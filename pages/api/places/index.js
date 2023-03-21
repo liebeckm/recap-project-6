ba86a352ce5c1f921247a4b01a955402136a8a43
Melissa Liebeck <melissaliebeck@aol.com>
2023-03-21 12:25:01 +0100

    initial push including branch

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,13 @@
-import { places } from '../../../lib/db';
+// new code starts here:
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
 }
